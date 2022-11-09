6d2fec7ca3fa1da3329a00c76ebb48d4485b0a5e
Sal <saleh.shehata@outlook.com>
2022-11-09 13:48:26 -0700

    Updates

---
+++ precache-manifest.10aea6297f02a0f159bb94ed63a8a743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7893ffce52aa02d55fb",
+    "url": "/static/css/main.8f5e3354.chunk.css"
+  },
+  {
+    "revision": "e7893ffce52aa02d55fb",
+    "url": "/static/js/main.e7893ffc.chunk.js"
+  },
+  {
+    "revision": "c9b44b48ec3ae4b88dff",
+    "url": "/static/js/1.c9b44b48.chunk.js"
+  },
+  {
+    "revision": "5f2695c65ff556e58a63",
+    "url": "/static/js/2.5f2695c6.chunk.js"
+  },
+  {
+    "revision": "3d6f8b640fae3d1f5dc2",
+    "url": "/static/js/runtime~main.3d6f8b64.js"
+  },
+  {
+    "revision": "c7f2aece29ff5d2672c87ee8fc782711",
+    "url": "/index.html"
+  }
+];
